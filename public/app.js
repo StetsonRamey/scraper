13440d19559c9d240491df060cba542f94370141
Stetson Ramey <stetson.ramey@gmail.com>
2018-05-29 17:51:07 -0500

    scrapes and saves to db

---
+++ public/app.js
@@ -0,0 +1,27 @@
+
+
+// TODO: write all this shit
+
+
+//TODO: write a .getJSON to get all the articles and load them into cards
+
+
+
+
+
+
+
+// TODO: write a on("click") to save an article
+
+
+// TODO: write a on("click") to delete an article
+
+
+
+// TODO: write a on("click") to save a comment
+
+
+
+// TODO: write a on("click") to delete a comment
+
+
--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 // DEPENDENCIES =======================================
 // =====================================================
 
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require('express');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 var path = require('path');
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
-var cheerio = require("cheerio");
+var axios = require('axios');
+var cheerio = require('cheerio');
 
 // Require all models
-var db = require("./models");
+var db = require('./models');
 
-var PORT = 3000;
+var PORT = 8080;
 
 // Initialize Express
 var app = express();
@@ -28,65 +29,123 @@ app.use(logger('dev'));
 // Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Connect to the Mongo DB
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/mongoRecodeScrape';
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
+const dbCheck = mongoose.connection;
+
+dbCheck.on('error', (error) => {
+  console.log('Mongoose Error: ', error);
+});
+
+dbCheck.once('open', () => {
+  console.log('Mongoose connection successful');
+});
 
 
 // TODO: ROUTES =======================================
 // =====================================================
 
+// GET ROUTES =================
+// ============================
+
+// GET for scraping our site
+app.get('/scrape', function(req, res) {
+  // GET for scraping our site
+  axios.get('http://recode.net').then(function(response) {
+    // load cheerio
+    var $ = cheerio.load(response.data);
+
+    $('div.c-entry-box--compact__body').each(function(i, element) {
+      var result = {};
+
+      result.headline = $(this)
+        .children('h2')
+        .children('a')
+        .text();
+      result.URL = $(this)
+        .children('h2')
+        .children('a')
+        .attr('href');
+      result.summary = $(this)
+        .children('.p-dek')
+        .text();
+      // console.log(result);
+      
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
+     
+    });
+
+    res.redirect('/');
+  });
+});
+
 // GET the html
-app.get('/saved', function (req, res) {
+app.get('/saved', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/saved.html'));
 });
 
-// TODO: GET for scraping our site
-app.get('/scrape', function (req, res) {
-
-})
-
 // GET for getting all articles from DB
-app.get('/articles', function (req, res) {
-  db.article.find({})
-    .then(function (dbArticle) {
+app.get('/articles', function(req, res) {
+  db.article
+    .find({})
+    .then(function(dbArticle) {
       res.json(dbArticle);
     })
-    .catch(function (err) {
+    .catch(function(err) {
       res.json(err);
     });
 });
 
 // GET route for getting a specific article and it's comments
-app.get('/articles/:id', function (req, res) {
-  db.article.findOne({ _id: req.params.id })
-  .populate('comment')
-  .then(function (dbArticle) {
-    res.json(dbArticle);
-  })
-  .catch(function (err) {
-    res.json(err);
-  });
-});
-
-// POST route for updating and saving a comment
-app.post('/articles/:id', function (req, res) {
-  db.comment.create(req.body)
-    .then(function (dbComment) {
-      return db.article.findOneAndUpdate({ _id: req.params.id }, {comment: dbComment._id}, { new: true });
-    })
-    .then(function (dbArticle) {
+app.get('/articles/:id', function(req, res) {
+  db.article
+    .findOne({ _id: req.params.id })
+    .populate('comment')
+    .then(function(dbArticle) {
       res.json(dbArticle);
     })
-    .catch(function (err) {
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+// POST ROUTES =================
+// ============================
+
+// POST route for updating and saving a comment
+app.post('/articles/:id', function(req, res) {
+  db.comment
+    .create(req.body)
+    .then(function(dbComment) {
+      return db.article.findOneAndUpdate(
+        { _id: req.params.id },
+        { comment: dbComment._id },
+        { new: true }
+      );
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
       res.json(err);
     });
 });
@@ -95,5 +154,5 @@ app.post('/articles/:id', function (req, res) {
 // =====================================================
 
 app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+  console.log('App running on port ' + PORT + '!');
 });
